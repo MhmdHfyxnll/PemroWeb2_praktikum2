12d99ee731c70e8fce3c0436a4c35d900b0980b5
MhmdHfyxnll <hafiyainulhafiyainun@gmail.com>
2026-04-22 16:08:47 +0700

    Delete app directory

--- app/Views/template/admin_footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-
-<footer class="text-center mt-5 mb-3 text-muted">
-    <small>Admin Panel © <?= date('Y') ?></small>
-</footer>
-
-</body>
-</html>
--- app/Views/template/footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-
-<footer class="bg-dark text-white text-center p-3 mt-5">
-    <small>© <?= date('Y') ?> - Naufal Rafi Haryanto</small>
-</footer>
-
-</body>
-</html>
